c57ce7dbfcff1919cba81085476f6fe3a161f592
Pavlo Ivanko <ivankopavel@gmail.com>
2023-05-27 22:46:07 +0300

    get rate ready

--- currency_service/db/db.go
+++ currency_service/db/db.go
@@ -10,11 +10,14 @@ import(
 
 var	packageVersion string = "0.0.1"
 
+var db_in_memory bool = false
+
 func init() {
 	fmt.Println("..db package version ", packageVersion)
 	file, err:= os.OpenFile(config.Config.DB_PATH,os.O_CREATE, 0666)
     if err != nil {
-        log.Fatal(err)
+        fmt.Printf("Unable to create/open db file at  %s, in-memory db will be used\n",config.Config.DB_PATH)
+		db_in_memory = true
     } else {
 		fmt.Println("Database file created at ",config.Config.DB_PATH)
 	}
--- currency_service/users/users.go
+++ currency_service/users/users.go
@@ -2,11 +2,20 @@ package users
 import
 (
 	"fmt"
+//	"currency_service/config"
+	"currency_service/db"
+//	"currency_service/currency"
 )
 
 var	packageVersion string = "0.0.1"
 
 
+func AddUser(email string) error {
+	fmt.Println("AddUser",email)
+	return nil
+}
+
 func init() {
 	fmt.Println("..users package version ", packageVersion)
+	db.Init()
 }
