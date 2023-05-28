b7926cee1aad3fdafb6dda7d71ccc876d1608e7f
Pavlo Ivanko <ivankopavel@gmail.com>
2023-05-28 09:35:34 +0300

    added subscription

--- currency_service/config/config.go
+++ currency_service/config/config.go
@@ -5,6 +5,7 @@ import(
 //	"os"
 	"context"
 	"github.com/sethvargo/go-envconfig"
+	"github.com/joho/godotenv"
 
 )
 
@@ -17,14 +18,15 @@ type AppConfig struct {
 	SMTP_HOST string `env:"SMTP_HOST"`
 	SMTP_PORT string `env:"SMTP_PORT"`
 	SMTP_USER string `env:"SMTP_USER"`
-	SMTP_PASS string `env:"SMTP_PASS"`
+	SMTP_PASSWORD string `env:"SMTP_PASSWORD"`
 	SMTP_FROM string `env:"SMTP_FROM"`
 	LIVE_COIN_WATCH_API_KEY string `env:"LIVE_COIN_WATCH_API_KEY"`}
 
 var	Config AppConfig
 
 func init() {
-
+	// for local dev or can be injected in docker container
+	godotenv.Load(".env")
 	ctx := context.Background()
 
 	envconfig.Process(ctx, &Config)
--- currency_service/users/users.go
+++ currency_service/users/users.go
@@ -4,7 +4,8 @@ import
 	"fmt"
 	"log"
 	"currency_service/db"
-//	"currency_service/currency"
+	"net/smtp"
+	"currency_service/currency"
 )
 
 var	packageVersion string = "0.0.1"
@@ -24,6 +25,27 @@ func AddUser(email string) (string,bool) {
 func ValidateEmail(email string) bool {
 	return email!="" && len(email)>3 && len(email)<30
 }
+
+func SendRate() (string,bool) {
+	dbSession:= sessionManager.GetDBSession()		
+	users,err:= dbSession.GetSubscribedUsers()
+	if err!=nil{
+		return "not able to get subscribers",false
+	}
+	smtp_from:=config.Config.SMTP_FROM
+	smtp_password:=config.Config.SMTP_PASSWORD
+	smtp_user:=config.Config.SMTP_USER
+	smtp_host:=config.Config.SMTP_HOST
+	smtp_port:=config.Config.SMTP_PORT
+	if smtp_from=="" || smtp_password=="" || smtp_user=="" || smtp_host=="" || smtp_port=="" {
+		return "not able to send email, check smtp settings",false
+	}
+	auth := smtp.PlainAuth("", smtp_user, smtp_password, smtp_host)
+	for _,user:=range users {
+		err := smtp.SendMail(smtp_host+":"+smtp_port, auth, smtp_from, []string{user}, []byte("To: "+user+"\r\nSubject: Currency rate\r\n\r\n"+currency.GetRate()))
+	return "",true
+}
+
 
 func init() {
 	fmt.Println("..users package version ", packageVersion)
